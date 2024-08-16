365962913c878f835815799414abdeeb80290f03
anandhusb444 <anandhusb444@gmail.com>
2024-08-16 22:16:15 +0530

    structuer

--- src/Component/Inputbox.jsx
+++ src/Component/Inputbox.jsx
@@ -7,8 +7,19 @@ function Inputbox() {
     <div>
       <div className='divBoxParent'>
         <div className='first_child_div'>
-            <input></input>
 
+            <div className='child_div_1'>
+                <h4 className='h1_phone'>Enter Phone number</h4>
+            </div>
+
+            <div className='child_div_2'>
+              <p className='main_p'>Enter your phone number to recive a passcode for authentiction</p>
+              <input className='main_input'></input>
+            </div>
+            
+            <div className='button_div'>
+              <button className='main_button'>Continue</button>
+            </div>
         </div>
     
 
