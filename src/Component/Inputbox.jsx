a07275014cdaf37f9e6a51181e0c9a2a29348d54
anandhusb444 <anandhusb444@gmail.com>
2024-08-16 17:28:29 +0530

    show-calling-code + flag

---
+++ src/Component/Inputbox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../Style/Inputbox.css'
+
+
+function Inputbox() {
+  return (
+    <div>
+      <div className='divBoxParent'>
+        <div className='first_child_div'>
+            <input></input>
+
+        </div>
+    
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Inputbox
