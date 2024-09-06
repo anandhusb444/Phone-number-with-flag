2343a77a828bdda0be59d707b5ae3c749afa31eb
anandhusb444 <anandhusb444@gmail.com>
2024-09-07 00:04:49 +0530

    div style update

--- src/Component/Inputbox.jsx
+++ src/Component/Inputbox.jsx
@@ -9,16 +9,16 @@ function Inputbox() {
         <div className='first_child_div'>
           <div className='continer_div'>
           <div className='child_div_1'>
-                <h4 className='h1_phone'>Enter Phone number</h4>
+                <h4 className='h1_phone'>Enter phone number</h4>
             </div>
 
             <div className='child_div_2'>
-              <p className='main_p'>Enter your phone number to recive a passcode for authentiction</p>
+              <p className='main_p'>Enter your phone number to recive a<br/> passcode for authentiction</p>
               <input className='main_input'></input>
             </div>
             
             <div className='button_div'>
-              <p className='p_button'>By Continuing, you May receive an SMS for Verification. Message and the data rates may apply </p>
+              <p className='p_button'>By Continuing, you May receive an SMS for<br/> Verification. Message and the data rates may apply </p>
               <button className='main_button'>Continue</button>
             </div>
 
